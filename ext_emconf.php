b328787694d8311047ef8daa79139a44d92d728b
mmunos <marc.munos@mce.gouv.qc.ca>
2023-10-03 14:39:30 -0400

    Processing rector

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,29 +1,6 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
-    'title' => 'Qc Be Domain Color',
-    'description' => 'Allows to set custom bg color by domain',
-    'category' => 'be',
-    'author' => 'Quebec.ca',
-    'author_email' => 'techno@quebec.ca',
-    'state' => 'stable',
-    'internal' => '',
-    'uploadfolder' => '0',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'version' => '1.0.2',
-    'constraints' => array(
-        'depends' => array(
-            'php' => '7.4-8.2',
-            'typo3' => '10.4.0-11.5.99',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
-        ),
-    ),
-    'autoload' => [
-        'psr-4' => [
-            'QC\\QcBeDomainColor\\' => 'Classes'
-        ],
+$EM_CONF[$_EXTKEY] = ['title' => 'Qc Be Domain Color', 'description' => 'Allows to set custom bg color by domain', 'category' => 'be', 'author' => 'Quebec.ca', 'author_email' => 'techno@quebec.ca', 'state' => 'stable', 'version' => '1.0.2', 'constraints' => ['depends' => ['php' => '7.4-8.2', 'typo3' => '10.4.0-11.5.99'], 'conflicts' => [], 'suggests' => []], 'autoload' => [
+    'psr-4' => [
+        'QC\\QcBeDomainColor\\' => 'Classes'
     ],
-);
+]];
