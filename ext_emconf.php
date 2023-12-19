5c60da3bcfd6a88d6c81b9d0d6b69ab17d676c26
mmunos <marc.munos@mce.gouv.qc.ca>
2023-12-19 14:43:27 -0500

    Corrections graphique + suppression fichier caduc

--- ext_emconf.php
+++
@@ -1,6 +0,0 @@
-<?php
-$EM_CONF[$_EXTKEY] = ['title' => 'Qc Be Domain Color', 'description' => 'Allows to set custom bg color by domain', 'category' => 'be', 'author' => 'Quebec.ca', 'author_email' => 'techno@quebec.ca', 'state' => 'stable', 'version' => '1.0.2', 'constraints' => ['depends' => ['php' => '7.4-8.2', 'typo3' => '10.4.0-11.5.99'], 'conflicts' => [], 'suggests' => []], 'autoload' => [
-    'psr-4' => [
-        'QC\\QcBeDomainColor\\' => 'Classes'
-    ],
-]];
