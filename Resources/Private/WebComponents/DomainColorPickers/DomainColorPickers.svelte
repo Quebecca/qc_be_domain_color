05a45f63b651f7d5e15fa11bb14e67974663364a
Abderrazak Tigiti <abderrazak.tigiti@mce.gouv.qc.ca>
2024-04-17 13:19:36 +0100

    Add color picker form

--- Resources/Private/WebComponents/DomainColorPickers/DomainColorPickers.svelte
+++ Resources/Private/WebComponents/DomainColorPickers/DomainColorPickers.svelte
@@ -7,13 +7,110 @@
     }
 }}"/>
 <script>
-    export let
-        domainColors = {},
-        conf = {}
+    import {Color, ColorInput} from 'color-picker-svelte'
+    import {onMount} from 'svelte';
 
+    export let
+        domainColors = {}
+
+    let domainName = '';
+    $: isEmptyDomainName = domainName.trim() === "";
+
+    $: colors = {};
+
+    function addNewDomain() {
+        colors[domainName] = new Color("#CCC")
+        colors = colors;
+    }
+
+    function deleteDomainColor(key) {
+        delete colors[key];
+        colors = colors;
+    }
+
+    onMount(() => {
+        Object.entries(domainColors).forEach(([key, value]) => {
+            colors[key] = new Color(value)
+        });
+    });
 </script>
-{conf.title}
-<span class="red">foo</span>
-{#each Object.entries(domainColors) as [key,value]}
-    {key} : {value}
-{/each}
+<div class="container">
+    <div class="row">
+        <div class="form-group t3js-formengine-validation-marker
+                    t3js-formengine-palette-field checkbox-column col-sm-6 col-md-4">
+            <div class="formengine-field-item t3js-formengine-field-item ">
+                <div class="form-control-wrap">
+                    <div class="form-wizards-wrap">
+                        <div class="form-wizards-element">
+                            <input id="new-domain"
+                                   bind:value={domainName}
+                                   autocomplete="off"
+                                   placeholder="Valid regexp, e.g. prod, dev.*, etc"
+                                   class="new-domain form-control"
+                                   pattern="[A-Za-z]{3}"
+                            />
+                            <span class="error-message" style="color: red;">
+                              {domainName && !/^([A-Za-z]{3})$/.test(domainName) ? "Invalid regexp" : ""}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="form-group t3js-formengine-validation-marker
+                    t3js-formengine-palette-field checkbox-column  col-sm-6 col-md-4">
+            <div class="formengine-field-item t3js-formengine-field-item ">
+                <div class="form-control-wrap">
+                    <div class="btn-group">
+                        <button on:click={addNewDomain} disabled={isEmptyDomainName} class="btn btn-default">
+                            New domain
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {#each Object.entries(colors) as [key, colorC]}
+        <div class="d-flex mb-3">
+            <div class="form-control-wrap">
+                <div class="form-wizards-wrap">
+                    <div class="form-wizards-element pr-2">
+                        <input type="text" value="{key}" class="edit form-control">
+                    </div>
+                </div>
+            </div>
+            <div class="t3js-formengine-validation-marker  mr-2">
+                <div class="formengine-field-item t3js-formengine-field-item">
+                    <div class="form-control-wrap" style="max-width: 156px">
+                        <div class="form-wizards-wrap">
+                            <div class="form-wizards-element">
+                                <ColorInput bind:color={colors[key]} showAlphaSlider/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="t3js-formengine-validation-marker checkbox-column  col-md-2 col-sm-3">
+                <div class="formengine-field-item t3js-formengine-field-item">
+
+                    <div class="form-control-wrap">
+                        <div class="form-wizards-wrap">
+                            <div class="form-wizards-element">
+                                <button
+                                    class="btn btn-default  t3js-editform-delete-record"
+                                    on:click={() => deleteDomainColor(key)}
+                                    >
+                                    <span class="t3js-icon icon icon-size-small icon-state-default icon-actions-edit-delete" data-identifier="actions-edit-delete">
+                                        <span class="icon-markup">
+                                            <svg class="icon-color" role="img"><use xlink:href="./actions.svg#actions-delete"></use></svg>
+                                        </span>
+	                                </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    {/each}
+</div>
--- Resources/Private/WebComponents/DomainColorPickers/fixtures.js
+++ Resources/Private/WebComponents/DomainColorPickers/fixtures.js
@@ -1,5 +1,6 @@
 let  fixtures = {
-    "prod": "FF0000",
-    "dev":"00FF00"
+    "prod": "#f6f0dc",
+    "web5": "#f6f0sz",
+    "dev4" : "#e5d086"
 };
 export default fixtures;
