808eaea49c3638d1ca107f9de45b03f11ea8a5a4
Abderrazak Tigiti <abderrazak.tigiti@mce.gouv.qc.ca>
2024-04-25 12:15:37 +0100

    Code cleaning

--- Resources/Private/WebComponents/DomainColorPickers/dev/test.svelte
+++ Resources/Private/WebComponents/DomainColorPickers/dev/test.svelte
@@ -4,16 +4,12 @@
 }}' />
 
 <script>
-    // import Debug from "pgu_web_components/Resources/Private/Debug.svelte";
 
     import fixtures from "../fixtures.js";
     import conf from "../conf.js";
     import DomainColorPickers from "../DomainColorPickers.svelte";
 </script>
 
-<!--<DomainColorPickers-->
-<!--    conf={conf}-->
-<!--    domainColors={fixtures}/>-->
 
 <qc-domain-color-pickers
         data-conf='{JSON.stringify(conf)}'
